a2304c354fc5da380b220bd6d87881c648daa4a0
Philopateer Moheb <104172766+PhilopateerMoheb1@users.noreply.github.com>
2024-01-29 12:17:01 +0200

    Update toy.h

--- toy.h
+++ toy.h
@@ -1,20 +1,26 @@
+#pragma once
 // toy.h
 
 #ifndef TOY_H
 #define TOY_H
 
+#include <stdlib.h>
+
 #define TK_K 3
 #define TK_N 4
 #define TK_Q 97
 
-// Helper function for polynomial multiplication in Z97[X]/(X^4+1)
-static void toy_polmul_naive(short* dst, const short* a, const short* b, int add_to_dst);
+#define NEG(X) (TK_Q - (X))
 
-// Main API functions
+void toy_fill_small(short* buf, int n);
+void toy_polmul_naive(short* dst, const short* a, const short* b, int add);
+void toy_mulmv(short* dst, const short* mat, const short* vec);
+void toy_mulmTv(short* dst, const short* mat, const short* vec);
+void toy_dot(short* dst, const short* v1, const short* v2);
+void toy_add(short* dst, const short* v1, const short* v2, int count, int v2_neg);
+
 void toy_gen(short* A, short* t, short* s);
 void toy_enc(const short* A, const short* t, int plain, short* u, short* v);
 int toy_dec(const short* s, const short* u, const short* v);
-// Helper function to print vectors
-static void print_vector(const char* name, const short* vector, int size);
 
 #endif // TOY_H
